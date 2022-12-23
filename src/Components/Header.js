6d2eb3ef90f4dd4e311edcc42f596adad81b4975
hodong <fire13764@gmail.com>
2022-12-23 11:42:06 +0900

    Header data 이동 &  카테고리 추가

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,54 +1,9 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import { list, social } from "../Data/header_list";
 
 function Header() {
-  const list = [
-    {
-      id: 1,
-      name: "High Rating",
-      path: "minimum_rating=9",
-    },
-    {
-      id: 2,
-      name: "Romance",
-      path: "genre=romance",
-    },
-    {
-      id: 3,
-      name: "Thriller",
-      path: "genre=thriller",
-    },
-    {
-      id: 4,
-      name: "Animation",
-      path: "genre=animation",
-    },
-  ];
-
-  const social = [
-    {
-      id: 1,
-      href: "https://ko-kr.facebook.com/",
-      icon: "fa-brands fa-facebook",
-      target: "_blank",
-      rel: "noreferrer",
-    },
-    {
-      id: 2,
-      href: "https://twitter.com/i/flow/login",
-      icon: "fa-brands fa-twitter",
-      target: "_blank",
-      rel: "noreferrer",
-    },
-    {
-      id: 3,
-      href: "https://www.instagram.com/",
-      icon: "fa-brands fa-instagram",
-      target: "_blank",
-      rel: "noreferrer",
-    },
-  ];
   return (
     <header className={styles.container}>
       <h1>
---
+++ src/Data/header_list.js
@@ -0,0 +1,58 @@
+const list = [
+  {
+    id: 1,
+    name: "High Rating",
+    path: "minimum_rating=9",
+  },
+  {
+    id: 2,
+    name: "Romance",
+    path: "genre=romance",
+  },
+  {
+    id: 3,
+    name: "Thriller",
+    path: "genre=thriller",
+  },
+  {
+    id: 4,
+    name: "Action",
+    path: "genre=action",
+  },
+  {
+    id: 5,
+    name: "Animation",
+    path: "genre=animation",
+  },
+  {
+    id: 6,
+    name: "Comedy",
+    path: "genre=comedy",
+  },
+];
+
+const social = [
+  {
+    id: 1,
+    href: "https://ko-kr.facebook.com/",
+    icon: "fa-brands fa-facebook",
+    target: "_blank",
+    rel: "noreferrer",
+  },
+  {
+    id: 2,
+    href: "https://twitter.com/i/flow/login",
+    icon: "fa-brands fa-twitter",
+    target: "_blank",
+    rel: "noreferrer",
+  },
+  {
+    id: 3,
+    href: "https://www.instagram.com/",
+    icon: "fa-brands fa-instagram",
+    target: "_blank",
+    rel: "noreferrer",
+  },
+];
+
+export { list, social };
